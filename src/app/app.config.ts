6f94c8e0ca0f60a4669d4af2d9481f952dcb1203
Andrew <andreythstan25@gmail.com>
2026-03-19 15:29:51 +0200

    More styling

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideClientHydration(withEventReplay()),
+    provideRouter(
+      routes, 
+      withInMemoryScrolling({ scrollPositionRestoration: 'top' })
+    )
   ]
 };
